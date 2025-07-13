ae97944aa779bc109b8d6d94adb7424bc2f21c64
FasterManNoblleNumbat <fasterdevman@outlook.com>
2025-07-13 17:28:00 +0200

    v0.0.5
    bug fix change loadExtension to load

--- src/FasterdotSymfonyPermissionManagerBundle.php
+++ src/FasterdotSymfonyPermissionManagerBundle.php
@@ -17,7 +17,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class FasterdotSymfonyPermissionManagerBundle extends Bundle
 {
-    public function loadExtension(array $configs, ContainerBuilder $container): void
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
         $loader->load('services.yaml');
